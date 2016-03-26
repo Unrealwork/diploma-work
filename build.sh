bc9270ff5ae08b982b3873a74e863b94d756dadf
Igor Shmagrinskiy <unrealowork@gmail.com>
2016-03-26 11:19:24 +0300

    updated build script

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 cd text/thesis/
 xelatex -synctex=1 -interaction=nonstopmode diploma.tex
 xdg-open diploma.pdf &
+rm diploma.aux
+rm diploma.log
+rm diploma.out
+rm diploma.synctex.gz
+rm diploma.toc
 exit
