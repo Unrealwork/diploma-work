64877eb080f91a446141d0f19efeeecf99faa719
unrealwork <unrealwork@gmail.com>
2017-05-13 18:02:23 +0200

    restructure

--- build.sh
+++ build.sh
@@ -2,9 +2,5 @@
 cd text/thesis/
 xelatex -synctex=1 -interaction=nonstopmode diploma.tex
 xdg-open diploma.pdf &
-rm diploma.aux
-rm diploma.log
-rm diploma.out
-rm diploma.synctex.gz
-rm diploma.toc
+# rm diploma.aux diploma.log diploma.out diploma.synctex.gz diploma.toc
 exit
