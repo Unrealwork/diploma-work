6be9a4196283a40af48a0109d9aaacc7cd7ece44
unrealwork <unrealwork@gmail.com>
2017-05-16 13:56:01 +0300

    1.2, 1.3

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd text/thesis/
+cd src/thesis/
 xelatex -synctex=1 -interaction=nonstopmode diploma.tex
 xdg-open diploma.pdf &
 # rm diploma.aux diploma.log diploma.out diploma.synctex.gz diploma.toc
