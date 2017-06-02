d7fbe2916d39accd6d0e33f500d58d5d1b249541
unrealwork <unrealwork@gmail.com>
2017-06-02 12:41:01 +0300

    code review

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ rm diploma.aux diploma.log diploma.out diploma.synctex.gz diploma.toc diploma.bb
 xelatex -synctex=1 -interaction=nonstopmode diploma
 bibtex diploma
 xelatex -synctex=1 -interaction=nonstopmode diploma
+xelatex -synctex=1 -interaction=nonstopmode diploma
 xdg-open diploma.pdf
 exit
